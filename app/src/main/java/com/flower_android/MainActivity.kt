f340440fc22e9bd7104997c4f99d5e8331558fe2
unso99 <unso99@naver.com>
2024-03-12 16:20:22 +0900

    Add: tabLayout과 viewPager2 연결
    tabLayout과 viewPager2 연결
    
    Resolves: #4

--- app/src/main/java/com/flower_android/MainActivity.kt
+++ app/src/main/java/com/flower_android/MainActivity.kt
@@ -2,10 +2,36 @@ package com.flower_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.flower_android.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val allFragment = AllFragment()
+    private val list = listOf(allFragment, OneFlowerFragment(), BouquetFragment(), BasketFragment())
+    private val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle, list)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.view = this
+        initView()
+    }
+
+    private fun initView() {
+        binding.viewPager.adapter = adapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = if(list[position] is AllFragment) {
+                "전체"
+            }else if(list[position] is OneFlowerFragment) {
+                "한송이"
+            }else if(list[position] is BouquetFragment) {
+                "꽃다발"
+            }else {
+                "바구니"
+            }
+        }.attach()
     }
 }
